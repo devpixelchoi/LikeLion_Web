a5cb7c7a0512f83da3ef2cc67665108987f26e63
devpixelchoi <85727063+devpixelchoi@users.noreply.github.com>
2021-06-15 15:15:11 +0900

    Java_Script_LikeLion
    baic JAVA Script training

---
+++ 0615_js_pro/myScript_2.js
@@ -0,0 +1,6 @@
+function myTst() {
+    document.getElementById("test").innerHTML = "This is test for JS Connect";
+    document.getElementById("test").style.backgroundColor = "#F26CB9";
+    document.getElementById("test").style.color = "white"
+}
+
