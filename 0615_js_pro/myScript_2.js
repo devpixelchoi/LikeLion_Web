223c2573fc872ca76f8dc66a6adaf538e93cd676
devpixelchoi <85727063+devpixelchoi@users.noreply.github.com>
2021-06-15 15:22:44 +0900

    Delete 0615_js_pro directory

--- 0615_js_pro/myScript_2.js
+++
@@ -1,6 +0,0 @@
-function myTst() {
-    document.getElementById("test").innerHTML = "This is test for JS Connect";
-    document.getElementById("test").style.backgroundColor = "#F26CB9";
-    document.getElementById("test").style.color = "white"
-}
-
